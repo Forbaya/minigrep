3f73fec5ccdc12df4286d280143a1c67bde51929
Ville <vilunen@gmail.com>
2020-06-21 15:38:26 +0300

    Added search function and tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,54 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_config_not_enough_arguments_err() {
+        let args = [String::from("minigrep")];
+        let config = Config::new(&args);
+
+        assert!(config.is_err(), "Not enough arguments.");
+    }
+
+    #[test]
+    fn new_config_is_ok() {
+        let args = [
+            String::from("minigrep"),
+            String::from("search"),
+            String::from("poem.txt")
+        ];
+
+        let config = Config::new(&args);
+
+        assert_eq!(config.is_ok(), true);
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+            Rust:\n\
+            safe, fast, productive.\n\
+            Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
